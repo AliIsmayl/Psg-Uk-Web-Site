6f85b8f506b05e65c469226a7ffdf02ecc749e26
AliIsmayl <ali.ismayil.681@gmail.com>
2024-09-24 17:31:12 +0400

    normal commit

--- Psg-UK/src/Components/Header/Header.jsx
+++ Psg-UK/src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.scss";
-import Image from "../../../Image/Header.jpg";
+import Image from "../../../Image/Header.webp";
 
 function Header() {
   return (
